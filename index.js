1b50c1044bde93cd859f3dc457e19b17a43ee3d1
Lee SH <85441537+mleeskr@users.noreply.github.com>
2025-04-24 23:43:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const url = 'mongodb://localhost:27017';
-const dbName = 'GrabDB'; // Database name (case-sensitive)
+const dbName = 'rideHailingDB'; // Database name (case-sensitive)
 let db, ridesCollection, usersCollection;
 
 // Initialize database connection
